642a33d82d7ceba456242a0392228da6aa0b1ed3
prajjawalsingh302050 <146240607+prajjawalsingh302050@users.noreply.github.com>
2024-08-31 17:41:55 +0000

    lect3

---
+++ LECT_4_modules/app.js
@@ -0,0 +1,6 @@
+export const sum=(a,b)=>{
+    console.log(`sum of ${a} and ${b} is equal to ${a+b}`)
+}
+export const sub=(a,b)=>{
+    console.log(`sub of ${a} and ${b} is equal to ${a-b}`)
+}
---
+++ LECT_4_modules/index.js
@@ -0,0 +1,4 @@
+console.log("this is Lect 4")
+import {sum,sub} from './app.js'
+sum(4,6)
+sub(50,30);
